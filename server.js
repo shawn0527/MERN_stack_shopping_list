b6b61d3763fbcf87d187824f77ee59b528b7cc86
shawn0527 <shawnren527@gmail.com>
2019-11-10 15:29:47 -0600

    Create server

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express') //backend framework
 const mongoose = require('mongoose') //ORM to interact with MongoDB database
-const bodyParser = require('bodyParser') //take request and get data from the body
+const bodyParser = require('body-parser') //take request and get data from the body
+
+const app =  express();
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/key").mongoURI;
+
+//Connect to Mongo
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => console.log(`Server start on port ${port}`));
+
